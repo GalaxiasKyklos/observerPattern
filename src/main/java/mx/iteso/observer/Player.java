6ad9791147ecb2b83a80ed1029d10494e589feae
GalaxiasKyklos <saul_enrique-7285-96@hotmail.com>
2016-09-22 07:35:05 -0500

    gg0

---
+++ src/main/java/mx/iteso/observer/Player.java
@@ -0,0 +1,10 @@
+package mx.iteso.observer;
+
+/**
+ * Created by Saúl on 22/09/2016.
+ */
+public class Player {
+    public String name;
+    public int number;
+    
+}
--- src/main/java/mx/iteso/observer/impl/StoreMonitorDisplay.java
+++ src/main/java/mx/iteso/observer/impl/StoreMonitorDisplay.java
@@ -2,7 +2,10 @@ package mx.iteso.observer.impl;
 
 import mx.iteso.observer.Displayable;
 import mx.iteso.observer.Observer;
+import mx.iteso.observer.Player;
 import mx.iteso.observer.Subject;
+
+import java.util.ArrayList;
 
 public class StoreMonitorDisplay implements Observer, Displayable {
     private String homeTeam;
@@ -10,6 +13,7 @@ public class StoreMonitorDisplay implements Observer, Displayable {
     private int homeGoals;
     private int awayGoals;
     private Subject scoresData;
+    private ArrayList<Player> players;
 
     public StoreMonitorDisplay(Subject scoresData){
         this.scoresData = scoresData;
@@ -22,11 +26,12 @@ public class StoreMonitorDisplay implements Observer, Displayable {
                 + awayTeam + " (AWAY) " + awayGoals);
     }
 
-    public void update(String home, String away, int homeGoals, int awayGoals) {
+    public void update(String home, String away, int homeGoals, int awayGoals, ArrayList<Player> players) {
         this.homeTeam = home;
         this.awayTeam = away;
         this.homeGoals = homeGoals;
         this.awayGoals = awayGoals;
+        this.players = players;
         display();
     }
 
