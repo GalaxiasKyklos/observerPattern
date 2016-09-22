a4bee9788a3e26fd73cda5d1c5f466dfc3045a6a
GalaxiasKyklos <saul_enrique-7285-96@hotmail.com>
2016-09-22 08:41:33 -0500

    gg9 Now with more test! ;)

--- src/test/java/mx/iteso/observer/impl/ScoresDataTest.java
+++ src/test/java/mx/iteso/observer/impl/ScoresDataTest.java
@@ -1,21 +1,32 @@
 package mx.iteso.observer.impl;
 
 import mx.iteso.observer.Observer;
+import mx.iteso.observer.Player;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
+import static mx.iteso.observer.Player.NUMPOSITIONS;
 
 public class ScoresDataTest {
     ScoresData scoresData;
     Observer observer;
+    ArrayList<Player> players;
 
     @Before
     public void setUp() {
         scoresData = new ScoresData();
         observer = mock(Observer.class);
+        players.add(new Player(
+                "Name",
+                0,
+                NUMPOSITIONS.GOALKEEPER.ordinal(),
+                "Team"
+        ));
     }
 
     @Test(expected=ArrayIndexOutOfBoundsException.class)
@@ -27,9 +38,9 @@ public class ScoresDataTest {
 
     @Test
     public void testSetScore() {
-        /*scoresData.registerObserver(observer);
         scoresData.registerObserver(observer);
-        scoresData.setScore("testTeam", "testTeam2", 1, 0);
-        verify(observer, times(2)).update("testTeam", "testTeam2", 1, 0);*/
+        scoresData.registerObserver(observer);
+        scoresData.setScore("testTeam", "testTeam2", 1, 0, players);
+        verify(observer, times(2)).update("testTeam", "testTeam2", 1, 0, players);
     }
 }
